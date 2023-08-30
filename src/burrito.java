f7ee8db9e7b19eb7fb18e49338c1d5c9c6a5b470
Josh <joshsimfukweemail@gmail.com>
2023-08-30 09:12:27 -0500

    The initial push of the first project

--- src/burrito.java
+++ src/burrito.java
@@ -1,5 +1,8 @@
 public class burrito {
     public static void main(String[] args) {
         System.out.println("Good morning USA Today is gonna be a wonderful wonderful day!");
+        System.out.print("Where are you?");
+        System.out.println("Ok buddy");
+
     }
 }
